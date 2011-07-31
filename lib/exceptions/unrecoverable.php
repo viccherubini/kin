a2e1770c50f24cb599f87b248492e380cff942b2
Vic Cherubini <vmc@leftnode.com>
2011-07-31 05:13:39 -0500

    New directory to store exceptions.

---
+++ lib/exceptions/unrecoverable.php
@@ -0,0 +1,8 @@
+<?php namespace jolt\exception;
+declare(encoding='UTF-8');
+
+class unrecoverable extends \Exception {
+	public function __construct($message) {
+		parent::__construct("[Unrecoverable Exception] {$message}");
+	}
+}
