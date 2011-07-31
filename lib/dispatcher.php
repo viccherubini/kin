a259ddba20b2b577715e907000e2fad29f4da8c7
Vic Cherubini <vmc@leftnode.com>
2011-07-31 11:55:41 -0500

    Refactored to be really clean.

--- lib/dispatcher.php
+++ lib/dispatcher.php
@@ -8,6 +8,7 @@ class dispatcher {
 	private $arguments = array();
 
 	private $action = null;
+	private $class = null;
 	private $controller = null;
 
 	public function __construct() {
@@ -22,38 +23,19 @@ class dispatcher {
 	
 	public function attach_controller(controller $controller) {
 		$this->controller = $controller;
+		$this->class = get_class($controller);
 		return $this;
 	}
 	
 	
 	
 	public function dispatch() {
-		if (is_null($this->controller)) {
-			throw new \jolt\exception\unrecoverable("The dispatcher must have a controller object attached before it can begin dispatching.");
-		}
+		$this->check_controller()
+			->check_action();
 		
-		if (empty($this->action)) {
-			throw new \jolt\exception\unrecoverable("The dispatcher must have a controller action set before it can begin dispatching.");
-		}
-
-		$class = get_class($this->controller);
-		try {
-			$action = new \ReflectionMethod($this->controller, $this->action);
-		} catch (\ReflectionException $e) {
-			throw new \jolt\exception\unrecoverable("The controller action, {$this->action}, is not a member of the controller class {$class}.");
-		}
-		
-		if (!$action->isPublic()) {
-			throw new \jolt\exception\unrecoverable("The controller action, {$this->action}, is not a public member of the controller class {$class}.");
-		}
-		
-		try {
-			$action_value = $action->invokeArgs($this->controller, $this->arguments);
-		} catch (\Exception $e) {
-			throw new \jolt\exception\unrecoverable("The controller action, {$this->action}, threw an exception that was not caught: ".$e->getMessage());
-		}
-		
-		return $action_value;
+		$action = $this->build_action();
+		return($this->check_action_is_public($action)
+			->dispatch_action($action));
 	}
 	
 	
@@ -72,6 +54,46 @@ class dispatcher {
 	
 	public function get_controller() {
 		return $this->controller;
+	}
+	
+	
+	
+	private function check_controller() {
+		if (is_null($this->controller)) {
+			throw new \jolt\exception\unrecoverable("The dispatcher must have a controller object attached before it can begin dispatching.");
+		}
+		return $this;
+	}
+	
+	private function check_action() {
+		if (empty($this->action)) {
+			throw new \jolt\exception\unrecoverable("The dispatcher must have a controller action set before it can begin dispatching.");
+		}
+		return $this;
+	}
+	
+	private function check_action_is_public($action) {
+		if (!$action->isPublic()) {
+			throw new \jolt\exception\unrecoverable("The controller action, {$this->action}, is not a public member of the controller class {$this->class}.");
+		}
+		return $this;
+	}
+	
+	private function build_action() {
+		try {
+			$action = new \ReflectionMethod($this->controller, $this->action);
+		} catch (\ReflectionException $e) {
+			throw new \jolt\exception\unrecoverable("The controller action, {$this->action}, is not a member of the controller class {$this->class}.");
+		}
+		return $action;
+	}
+	
+	private function dispatch_action($action) {
+		try {
+			return($action->invokeArgs($this->controller, $this->arguments));
+		} catch (\Exception $e) {
+			throw new \jolt\exception\unrecoverable("The controller action, {$this->action}, threw an exception that was not caught: ".$e->getMessage());
+		}
 	}
 	
 }
