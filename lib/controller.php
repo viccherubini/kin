beda6745b861142d582712dbcdbcaf4e4a4db06b
Vic Cherubini <vmc@leftnode.com>
2011-07-31 13:12:53 -0500

    The basic controller for executing actions.

---
+++ lib/controller.php
@@ -0,0 +1,104 @@
+<?php namespace jolt;
+declare(encoding='UTF-8');
+
+class controller {
+	
+	private $headers = array();
+	private $payload = array();
+	
+	private $response_code = 200;
+	private $view = null;
+	
+	public function __construct() {
+		$this->payload = array(
+			'contents' => array(),
+			'errors' => array(),
+			'models' => array(),
+			'message' => '',
+			'redirect' => ''
+		);
+	}
+	
+	public function __destruct() {
+		$this->payload = array();
+	}
+	
+	public function __set($k, $v) {
+		$this->payload['contents'][$k] = $v;
+		return $this;
+	}
+	
+	public function __get($k) {
+		if (array_key_exists($k, $this->payload['contents'])) {
+			return $this->payload['contents'][$k];
+		}
+		return null;
+	}
+	
+	public function add_header($header, $value) {
+		$header = strtolower(trim($header));
+		if ('content-type' !== $header) {
+			$this->headers[$header] = $value;
+		}
+		return $this;
+	}
+	
+	public function add_error($field, $error) {
+		$field = trim($field);
+		$this->payload['errors'][$field] = trim($error);
+		return $this;
+	}
+	
+	public function add_message($e) {
+		$message = $e;
+		if (is_object($e) && ($e instanceof \Exception)) {
+			$message = $e->getMessage();
+		}
+		$this->payload['message'] = $message;
+		return $this;
+	}
+
+	public function add_model(model $model) {
+		if (is_object($model)) {
+			$this->payload['models'][] = array(get_class($model), $model->get_values());
+		}
+		return $this;
+	}
+	
+	public function add_models(array $models) {
+		foreach ($models as $model) {
+			$this->add_model($model);
+		}
+		return $this;
+	}
+	
+	public function register($k, $v) {
+		return $this->__set($k, $v);
+	}
+	
+	public function render($view) {
+		$this->view = trim($view);
+	}
+
+	
+	
+	public function set_response_code($response_code) {
+		$this->response_code = (int)$response_code;
+		return $this;
+	}
+	
+	
+	
+	public function get_headers() {
+		return $this->headers;
+	}
+	
+	public function get_response_code() {
+		return $this->response_code;
+	}
+	
+	public function get_payload() {
+		return $this->payload;
+	}
+	
+}
---
+++ tests/controller_test.php
@@ -0,0 +1,119 @@
+<?php namespace jolt_test;
+declare(encoding='UTF-8');
+
+use \jolt\controller as controller;
+
+require_once(__DIR__.'/testcase.php');
+require_once(__DIR__.'/../lib/controller.php');
+
+class controller_test extends testcase {
+	
+	public function test___set__adds_to_contents() {
+		$value = uniqid();
+		
+		$controller = new controller;
+		$controller->field = $value;
+		
+		$payload = $controller->get_payload();
+		$this->assertEquals($value, $payload['contents']['field']);
+	}
+	
+	public function test___get__returns_from_contents() {
+		$value = uniqid();
+		
+		$controller = new controller;
+		$controller->field = $value;
+		
+		$this->assertEquals($value, $controller->field);
+	}
+	
+	public function test_add_header__header_cannot_be_content_type() {
+		$controller = new controller;
+		$controller->add_header('content-type', 'text/html');
+		
+		$this->assertEmpty($controller->get_headers());
+	}
+	
+	public function test_add_header__header_is_lowercase() {
+		$header_uppercase = 'X-JOLT-VERSION';
+		$header_lowercase = strtolower($header_uppercase);
+		$value = '1.0.0';
+		
+		$controller = new controller;
+		$controller->add_header($header_uppercase, $value);
+		
+		$headers = $controller->get_headers();
+		$this->assertEquals($header_lowercase, key($headers));
+	}
+	
+	public function test_add_error__error_added_to_stack() {
+		$field = 'username';
+		$error = 'Username can not be empty';
+		
+		$controller = new controller;
+		$controller->add_error($field, $error);
+		
+		$payload = $controller->get_payload();
+		$this->assertEquals($payload['errors'][$field], $error);
+	}
+	
+	/**
+	 * @expectedException \PHPUnit_Framework_Error
+	 */
+	public function test_add_model__requires_model_object() {
+		$controller = new controller;
+		
+		$controller->add_model('hello, world');
+	}
+	
+	public function test_add_model__adds_model_name() {
+		$model_class = 'jolt\model';
+		
+		$model = $this->getMock($model_class, array('get_values'));
+		$model->expects($this->once())
+			->method('get_values')
+			->will($this->returnValue(array()));
+		
+		$controller = new controller;
+		$controller->add_model($model);
+		
+		$payload = $controller->get_payload();
+		$this->assertEquals($model_class, get_parent_class($payload['models'][0][0]));
+	}
+	
+	public function test_add_model__adds_model_values() {
+		$model_values = array('id' => mt_rand(1, 1000), 'password' => uniqid());
+		
+		$model = $this->getMock($model_class, array('get_values'));
+		$model->expects($this->once())
+			->method('get_values')
+			->will($this->returnValue($model_values));
+		
+		$controller = new controller;
+		$controller->add_model($model);
+		
+		$payload = $controller->get_payload();
+		$this->assertEquals($model_values, $payload['models'][0][1]);
+	}
+	
+	/**
+	 * @expectedException \PHPUnit_Framework_Error
+	 */
+	public function test_add_models__requires_array() {
+		$controller = new controller;
+		
+		$controller->add_models(null);
+	}
+	
+	public function test_register__adds_to_contents() {
+		$field = 'password';
+		$value = uniqid();
+		
+		$controller = new controller;
+		$controller->register($field, $value);
+		
+		$payload = $controller->get_payload();
+		$this->assertEquals($value, $payload['contents'][$field]);
+	}
+	
+}
