8b242b461df7485528678c1317813cdff81ffec9
Vic Cherubini <vmc@leftnode.com>
2011-07-31 06:50:45 -0500

    The compiler and it's tests are now complete, it can find a controller, build it, and return it.

--- lib/compiler.php
+++ lib/compiler.php
@@ -1,12 +1,16 @@
 <?php namespace jolt;
 declare(encoding='UTF-8');
 
+require_once(__DIR__.'/exceptions/unrecoverable.php');
+
 class compiler {
 
 	private $class = '';
 	private $file = '';
 	private $path = '';
 
+	private $controller = null;
+	
 	public function __construct() {
 	
 	}
@@ -18,7 +22,23 @@ class compiler {
 
 
 	public function compile() {
+		$this->check_class()
+			->check_file();
 		
+		$file_path = $this->path.$this->file;
+		if (!is_file($file_path)) {
+			throw new \jolt\exception\unrecoverable("The compiler can not find the controller file, {$file_path}. Compilation can not continue.");
+		}
+		
+		require_once($file_path);
+		if (!class_exists($this->class, false)) {
+			throw new \jolt\exception\unrecoverable("The compiler can not find the controller class, {$this->class} in the controller file, {$file_path}. Compilation can not continue.");
+		}
+		
+		$ref = new \ReflectionClass($this->class);
+		$this->controller = $ref->newInstance();
+		
+		return true;
 	}
 	
 	
@@ -34,12 +54,30 @@ class compiler {
 	}
 	
 	public function set_path($path) {
-		$this->path = trim($path);
+		$this->path = rtrim($path, DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR;
 		return $this;
 	}
 
 
 
+	public function get_controller() {
+		return $this->controller;
+	}
 	
-
+	
+	
+	private function check_class() {
+		if (empty($this->class)) {
+			throw new \jolt\exception\unrecoverable("The compiler must have a controller class before it can begin compilation.");
+		}
+		return $this;
+	}
+	
+	private function check_file() {
+		if (empty($this->file)) {
+			throw new \jolt\exception\unrecoverable("The compiler must have a controller file before it can begin compilation.");
+		}
+		return $this;
+	}
+	
 }
--- tests/compiler_test.php
+++ tests/compiler_test.php
@@ -1,33 +1,93 @@
 <?php namespace jolt_test;
 declare(encoding='UTF-8');
 
-use \jolt\app as app;
+use \jolt\compiler as compiler;
+
+require_once('vfsStream/vfsStream.php');
 
 require_once(__DIR__.'/testcase.php');
-require_once(__DIR__.'/../lib/app.php');
+require_once(__DIR__.'/../lib/compiler.php');
 
-class app_test extends testcase {
+class compiler_test extends testcase {
 	
-	public function test___construct__builds_compiler() {
-		$app = new app;
-		$this->assertInstanceOf('\jolt\compiler', $app->compiler);
+	private $class = 'index_controller';
+	private $path = 'controllers';
+
+	/**
+	 * @expectedException \jolt\exception\unrecoverable
+	 */
+	public function test_compile__requires_class() {
+		$compiler = new compiler;
+		
+		$compiler->compile();
 	}
 	
-	public function test___construct__builds_request() {
-		$app = new app;
-		$this->assertInstanceOf('\jolt\request', $app->request);
+	/**
+	 * @expectedException \jolt\exception\unrecoverable
+	 */
+	public function test_compile__requires_file() {
+		$compiler = new compiler;
+		$compiler->set_class($this->class);
+		
+		$compiler->compile();
 	}
 	
-	public function test___construct__builds_response() {
-		$app = new app;
-		$this->assertInstanceOf('\jolt\response', $app->response);
+	/**
+	 * @expectedException \jolt\exception\unrecoverable
+	 */
+	public function test_compile__requires_file_to_exist() {
+		$compiler = new compiler;
+		$compiler->set_class($this->class)
+			->set_path($this->path)
+			->set_file($this->get_file());
+		
+		$compiler->compile();
 	}
 	
-	public function test___construct__builds_router() {
-		$app = new app;
-		$this->assertInstanceOf('\jolt\router', $app->router);
+	/**
+	 * @expectedException \jolt\exception\unrecoverable
+	 */
+	public function test_compile__requires_class_to_exist() {
+		$file = $this->get_file();
+		
+		\vfsStreamWrapper::register();
+		\vfsStreamWrapper::setRoot(new \vfsStreamDirectory($this->path));
+
+		$path = \vfsStreamWrapper::getRoot();
+		$path->addChild(\vfsStream::newFile($file)->withContent('<?php class abc { }'));
+
+		$file_url = \vfsStream::url($this->path.DIRECTORY_SEPARATOR.$file);
+
+		$compiler = new compiler;
+		$compiler->set_class($this->class)
+			->set_file($file_url);
+
+		$compiler->compile();
+	}
+	
+	public function test_compile__controller_built() {
+		$file = $this->get_file();
+		
+		\vfsStreamWrapper::register();
+		\vfsStreamWrapper::setRoot(new \vfsStreamDirectory($this->path));
+
+		$path = \vfsStreamWrapper::getRoot();
+		$path->addChild(\vfsStream::newFile($file)->withContent('<?php class '.$this->class.' { }'));
+
+		$file_url = \vfsStream::url($this->path.DIRECTORY_SEPARATOR.$file);
+		
+		$compiler = new compiler;
+		$compiler->set_class($this->class)
+			->set_file($file_url);
+			
+		$compiler->compile();
+
+		$this->assertTrue(is_object($compiler->get_controller()));
 	}
 	
 	
 	
+	private function get_file() {
+		return uniqid(true).'_'.$this->class.'.php';
+	}
 }
