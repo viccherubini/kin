eedd2e157dea1a6670b9ee1461e7bf6becf34442
Vic Cherubini <vmc@leftnode.com>
2011-07-31 05:14:16 -0500

    The constructor builds a new route and started to flesh out the idea of the execute() method.

--- lib/app.php
+++ lib/app.php
@@ -3,15 +3,18 @@ declare(encoding='UTF-8');
 
 require_once(__DIR__.'/request.php');
 require_once(__DIR__.'/response.php');
+require_once(__DIR__.'/router.php');
 
 class app {
 
 	public $request = null;
 	public $response = null;
+	public $router = null;
 
 	public function __construct() {
 		$this->request = new request;
 		$this->response = new response;
+		$this->router = new router;
 	}
 	
 	public function __destruct() {
@@ -19,5 +22,12 @@ class app {
 	}
 
 
-	
+	public function execute() {
+		$this->request
+			->set_accept(filter_input(INPUT_SERVER, 'HTTP_ACCEPT'))
+			->set_method(filter_input(INPUT_SERVER, 'REQUEST_METHOD'))
+			->set_path(filter_input(INPUT_SERVER, 'PATH_INFO'));
+			
+		
+	}
 }
