143958578d7bfc3d1347511ed33eec863b1c7a8a
Vic Cherubini <vmc@leftnode.com>
2011-07-31 06:17:28 -0500

    Finished writing the router so it routes requests properly.

--- lib/router.php
+++ lib/router.php
@@ -5,14 +5,20 @@ require_once(__DIR__.'/exceptions/unrecoverable.php');
 
 class router {
 
+	// Data passed in from the request
 	private $path = '';
+	private $request_method = '';
 	
+	// All of the routes
+	private $compiled_routes = array();
+	private $uncompiled_routes = array();
+	private $error_route = array();
+	
+	// The matched route and arguments
 	private $arguments = array();
 	private $route = array();
-	private $routes = array();
-	private $error_route = array();
-
-	const alphanum_replacement = '([a-z0-9_\-/%\.\*]*)';
+	
+	const alphanum_replacement = '([a-z0-9_\-/%\.\*\+]*)';
 	const numeric_replacement = '([\d]+)';
 
 	public function __construct() {
@@ -26,26 +32,25 @@ class router {
 
 
 	public function route() {
-		$this->check_routes()
+		$this->check_compiled_routes()
 			->check_error_route();
 		
-		if (empty($this->path)) {
+		$matched_route = false;
+		if (empty($this->path) || empty($this->request_method)) {
 			$this->route = $this->error_route;
 			return true;
 		}
 		
-		
-		/*$routes_count = count($this->routes);
-		$matched_route = false;
+		$routes_count = count($this->compiled_routes);
 		for ($i=0; $i<$routes_count; $i++) {
-			$argv = array();
+			$arguments = array();
 			
-			$request_methods_match = ($this->request['routes'][$i][0] === $this->request['request_method']);
-			$routes_match = (preg_match_all($this->request['routes'][$i][1], $this->request['path'], $argv, PREG_SET_ORDER) > 0);
+			$request_methods_match = ($this->compiled_routes[$i][0] === $this->request_method);
+			$routes_match = (preg_match_all($this->compiled_routes[$i][1], $this->path, $arguments, PREG_SET_ORDER) > 0);
 			
 			if ($request_methods_match && $routes_match) {
-				$this->request['route']['route'] = $this->request['routes'][$i];
-				$this->request['route']['arguments'] = array_slice($argv[0], 1);
+				$this->route = $this->uncompiled_routes[$i];
+				$this->arguments = array_slice($arguments[0], 1);
 				
 				$matched_route = true;
 				break;
@@ -53,10 +58,10 @@ class router {
 		}
 		
 		if (!$matched_route) {
-			$this->request['route']['route'] = $this->request['route_404'];
-		}*/
+			$this->route = $this->error_route;
+		}
 		
-		
+		return true;
 	}
 
 
@@ -67,8 +72,8 @@ class router {
 		return $this;
 	}
 	
-	public function set_arguments(array $arguments) {
-		$this->arguments = $arguments;
+	public function set_request_method($request_method) {
+		$this->request_method = strtoupper($request_method);
 		return $this;
 	}
 	
@@ -78,32 +83,30 @@ class router {
 	}
 	
 	public function set_routes(array $routes) {
-		$search = array('%s', '%n');
-		$replace = array(self::alphanum_replacement, self::numeric_replacement);
-		
-		$this->routes = array_map(function($e) use ($search, $replace) {
-			$e[1] = str_replace($search, $replace, "#^{$e[1]}$#i");
-			return $e;
-		}, $routes);
-		
+		$this->uncompiled_routes = $routes;
+		$this->compiled_routes = $this->compile_routes($routes);
 		return $this;
 	}
 	
 	
 	
+	public function get_arguments() {
+		return $this->arguments;
+	}
+	
 	public function get_route() {
 		return $this->route;
 	}
 	
-	public function get_routes() {
-		return $this->routes;
+	public function get_compiled_routes() {
+		return $this->compiled_routes;
 	}
 	
 	
 	
-	private function check_routes() {
-		if (0 === count($this->routes)) {
-			throw new \jolt\exception\unrecoverable("The router must have at least one route before it can route a request.");
+	private function check_compiled_routes() {
+		if (0 === count($this->compiled_routes)) {
+			throw new \jolt\exception\unrecoverable("The router must have at least one compiled route before it can route a request.");
 		}
 		return $this;
 	}
@@ -114,4 +117,17 @@ class router {
 		}
 		return $this;
 	}
+	
+	private function compile_routes($routes) {
+		$search = array('%s', '%n');
+		$replace = array(self::alphanum_replacement, self::numeric_replacement);
+		
+		$routes = array_map(function($e) use ($search, $replace) {
+			$e[1] = str_replace($search, $replace, "#^{$e[1]}$#i");
+			return $e;
+		}, $routes);
+		
+		return $routes;
+	}
+	
 }
