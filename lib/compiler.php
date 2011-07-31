53e81de5ad838a8fcfc747e9ffbd848446025114
Vic Cherubini <vmc@leftnode.com>
2011-07-31 06:24:28 -0500

    Starting work on the compiler.

--- lib/compiler.php
+++ lib/compiler.php
@@ -3,6 +3,10 @@ declare(encoding='UTF-8');
 
 class compiler {
 
+	private $class = '';
+	private $file = '';
+	private $path = '';
+
 	public function __construct() {
 	
 	}
@@ -10,5 +14,32 @@ class compiler {
 	public function __destruct() {
 	
 	}
+
+
+
+	public function compile() {
+		
+	}
+	
+	
+	
+	public function set_class($class) {
+		$this->class = trim($class);
+		return $this;
+	}
+	
+	public function set_file($file) {
+		$this->file = trim($file);
+		return $this;
+	}
+	
+	public function set_path($path) {
+		$this->path = trim($path);
+		return $this;
+	}
+
+
+
+	
 
 }
---
+++ tests/compiler_test.php
@@ -0,0 +1,33 @@
+<?php namespace jolt_test;
+declare(encoding='UTF-8');
+
+use \jolt\app as app;
+
+require_once(__DIR__.'/testcase.php');
+require_once(__DIR__.'/../lib/app.php');
+
+class app_test extends testcase {
+	
+	public function test___construct__builds_compiler() {
+		$app = new app;
+		$this->assertInstanceOf('\jolt\compiler', $app->compiler);
+	}
+	
+	public function test___construct__builds_request() {
+		$app = new app;
+		$this->assertInstanceOf('\jolt\request', $app->request);
+	}
+	
+	public function test___construct__builds_response() {
+		$app = new app;
+		$this->assertInstanceOf('\jolt\response', $app->response);
+	}
+	
+	public function test___construct__builds_router() {
+		$app = new app;
+		$this->assertInstanceOf('\jolt\router', $app->router);
+	}
+	
+	
+	
+}
