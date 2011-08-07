9a272ebecedc2a3b4e915447da109d700639df5d
Vic Cherubini <vmc@leftnode.com>
2011-08-07 05:26:10 -0500

    Real tests

--- tests/unit/app_test.php
+++ tests/unit/app_test.php
@@ -1,15 +1,23 @@
 <?php namespace kintest;
 declare(encoding='UTF-8');
 
-use \jolt\app as app;
+use \kin\app as app;
 
 require_once(__DIR__.'/testcase.php');
 require_once(__DIR__.'/../../kin/app.php');
 
 class app_test extends testcase {
 	
-	public function test_true() {
-		$this->assertTrue(true);
+	public function test___construct__compiles_request() {
+		$app = new app;
+		
+		$this->assertInternalType('object', $app->request);
+	}
+	
+	public function test___construct__builds_response() {
+		$app = new app;
+		
+		$this->assertInternalType('object', $app->response);
 	}
 	
 }
