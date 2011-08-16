69d0960acfc5ce4306c1f4f272d759124480b41d
Vic Cherubini <vmc@leftnode.com>
2011-08-16 05:47:12 -0500

    The helper class and methods for writing css(), js(), img(), and url().

--- kin/lib/app/helper.php
+++ kin/lib/app/helper.php
@@ -3,6 +3,8 @@ declare(encoding='UTF-8');
 
 class helper {
 
+	private $root_url = '';
+	
 	private $settings = null;
 
 	public function __construct() {
@@ -13,6 +15,7 @@ class helper {
 	
 	public function attach_settings(settings $settings) {
 		$this->settings = $settings;
+		$this->compile_root_url();
 		return($this);
 	}
 	
@@ -20,18 +23,46 @@ class helper {
 	
 	public function css($css_file, $media='screen', $local_file=true) {
 		if ($local_file) {
-			$css_file = $this->append_extension($css_file, '.css');
-			//$root_url = $this->get_root_url();
-			//$css_file = $root_url.$this->css_path.$css_file;
+			$css_file = $this->root_url.$this->settings->css_path.$this->append_extension($css_file, '.css');
 		}
-
-		$link_tag = sprintf('<link type="text/css" rel="stylesheet" href="%s" media="%s">%s', $css_file, $media, PHP_EOL);
-		return $link_tag;
+		return(sprintf('<link type="text/css" rel="stylesheet" href="%s" media="%s">%s', $css_file, $media, PHP_EOL));
 	}
 	
+	public function js($js_file, $local_file=true) {
+		if ($local_file) {
+			$js_file = $this->root_url.$this->settings->js_path.$this->append_extension($js_file, '.js');
+		}
+		return(sprintf('<script src="%s" type="text/javascript"></script>%s', $js_file, PHP_EOL));
+	}
 	
+	public function img($img_src, $alt_text=null, $tag_attributes=null, $local_file=true) {
+		if ($local_file) {
+			$img_src = $this->root_url.$this->settings->images_path.$img_src;
+		}
+		return(sprintf('<img src="%s" alt="%s" title="%s" %s>%s', $img_src, $alt_text, $alt_text, $tag_attributes, PHP_EOL));
+	}
 	
-	
+	public function url() {
+		$argc = func_num_args();
+		$argv = func_get_args();
+
+		$http_parameters = '';
+		if ($argc > 1 && is_array($argv[$argc-1])) {
+			$http_parameters = http_build_query(array_pop($argv));
+		}
+		
+		$root_url = $this->root_url;
+		if (!$this->settings->rewrite) {
+			$root_url .= 'index.php/';
+		}
+		
+		$url = $root_url.implode('/', $argv);
+		if (!empty($http_parameters)) {
+			$url .= '?'.$http_parameters;
+		}
+
+		return($url);
+	}
 	
 	
 	
@@ -39,8 +70,18 @@ class helper {
 		if (0 == preg_match('/\\'.$ext.'$/i', $file)) {
 			$file .= $ext;
 		}
-		return $file;
+		return($file);
 	}
 
+	private function compile_root_url() {
+		$this->root_url = $this->settings->url;
+		if ($this->is_secure() || $this->settings->force_ssl) {
+			$this->root_url = $this->settings->secure_url;
+		}
+	}
+
+	private function is_secure() {
+		return('on' === strtolower(filter_input(INPUT_SERVER, 'HTTPS')));
+	}
 
 }
--- tests/unit/app/helper_test.php
+++ tests/unit/app/helper_test.php
@@ -21,21 +21,292 @@ class helper_test extends testcase {
 	
 	public function test_css__appends_extension_only_if_local_file() {
 		$settings = $this->getMock('\kin\app\settings');
+		$settings->force_ssl = false;
+		$settings->css_path = 'assets/css/';
+		$settings->url = 'http://localhost/';
 		
 		$helper = new helper;
 		$helper->attach_settings($settings);
 		
-		$css_file = '/path/to/css';
-		$css_file_with_extension = $css_file.'.css';
+		$local_css_file = 'reset';
+		$local_css_file_with_extension = $settings->url.$settings->css_path.$local_css_file.'.css';
 		
-		$link_tag = $helper->css($css_file);
+		$external_css_file = 'http://leftnode.com/path/to/css/reset';
 		
-		$matcher = array(
+		$this->assertTag(array(
 			'tag' => 'link',
-			'attributes' => array('href' => $css_file_with_extension)
-		);
+			'attributes' => array(
+				'href' => $local_css_file_with_extension
+			)
+		), $helper->css($local_css_file));
 		
-		$this->assertTag($matcher, $link_tag);
+		$this->assertTag(array(
+			'tag' => 'link',
+			'attributes' => array(
+				'href' => $external_css_file
+			)
+		), $helper->css($external_css_file, 'screen', false));
+	}
+	
+	public function test_css__appends_root_url_if_local_file() {
+		$settings = $this->getMock('\kin\app\settings');
+		$settings->force_ssl = false;
+		$settings->css_path = 'assets/css/';
+		$settings->url = 'http://leftnode.com/';
+		
+		$helper = new helper;
+		$helper->attach_settings($settings);
+		
+		$local_css_file = 'reset';
+		$local_css_file_full_url = $settings->url.$settings->css_path.$local_css_file.'.css';
+		
+		$this->assertTag(array(
+			'tag' => 'link',
+			'attributes' => array(
+				'href' => $local_css_file_full_url
+			)
+		), $helper->css($local_css_file));
+	}
+	
+	public function test_css__forces_ssl_url_if_local_file() {
+		$settings = $this->getMock('\kin\app\settings');
+		$settings->force_ssl = true;
+		$settings->css_path = 'assets/css/';
+		$settings->url = 'http://leftnode.com/';
+		$settings->secure_url = 'https://leftnode.com/';
+		
+		$helper = new helper;
+		$helper->attach_settings($settings);
+		
+		$local_css_file = 'reset';
+		$local_css_file_full_url = $settings->secure_url.$settings->css_path.$local_css_file.'.css';
+		
+		$this->assertTag(array(
+			'tag' => 'link',
+			'attributes' => array(
+				'href' => $local_css_file_full_url
+			)
+		), $helper->css($local_css_file));
+	}
+	
+	
+	
+	public function test_js_appends_extension_only_if_local_file() {
+		$settings = $this->getMock('\kin\app\settings');
+		$settings->force_ssl = false;
+		$settings->js_path = 'assets/js/';
+		$settings->url = 'http://localhost/';
+		
+		$helper = new helper;
+		$helper->attach_settings($settings);
+		
+		$local_js_file = 'jquery-1.6.2';
+		$local_js_file_with_extension = $settings->url.$settings->js_path.$local_js_file.'.js';
+		
+		$external_js_file = 'http://leftnode.com/assets/js/jquery-1.6.2';
+		
+		$this->assertTag(array(
+			'tag' => 'script',
+			'attributes' => array(
+				'src' => $local_js_file_with_extension
+			)
+		), $helper->js($local_js_file));
+		
+		$this->assertTag(array(
+			'tag' => 'script',
+			'attributes' => array(
+				'src' => $external_js_file
+			)
+		), $helper->js($external_js_file, false));
+	}
+	
+	public function test_js__appends_root_url_if_local_file() {
+		$settings = $this->getMock('\kin\app\settings');
+		$settings->force_ssl = false;
+		$settings->js_path = 'assets/js/';
+		$settings->url = 'http://leftnode.com/';
+		
+		$helper = new helper;
+		$helper->attach_settings($settings);
+		
+		$local_js_file = 'jquery-1.6.2';
+		$local_js_file_full_url = $settings->url.$settings->js_path.$local_js_file.'.js';
+		
+		$this->assertTag(array(
+			'tag' => 'script',
+			'attributes' => array(
+				'src' => $local_js_file_full_url
+			)
+		), $helper->js($local_js_file));
+	}
+	
+	public function test_js__forces_ssl_url_if_local_file() {
+		$settings = $this->getMock('\kin\app\settings');
+		$settings->force_ssl = true;
+		$settings->js_path = 'assets/js/';
+		$settings->url = 'http://leftnode.com/';
+		$settings->secure_url = 'https://leftnode.com/';
+		
+		$helper = new helper;
+		$helper->attach_settings($settings);
+		
+		$local_js_file = 'jquery-1.6.2';
+		$local_js_file_full_url = $settings->secure_url.$settings->js_path.$local_js_file.'.js';
+		
+		$this->assertTag(array(
+			'tag' => 'script',
+			'attributes' => array(
+				'src' => $local_js_file_full_url
+			)
+		), $helper->js($local_js_file));
+	}
+	
+	
+	
+	public function test_img__appends_root_url_if_local_file() {
+		$settings = $this->getMock('\kin\app\settings');
+		$settings->force_ssl = false;
+		$settings->images_path = 'assets/images/';
+		$settings->url = 'http://leftnode.com/';
+		
+		$helper = new helper;
+		$helper->attach_settings($settings);
+		
+		$local_image_file = 'logo.png';
+		$local_image_file_full_url = $settings->url.$settings->images_path.$local_image_file;
+		
+		$this->assertTag(array(
+			'tag' => 'img',
+			'attributes' => array(
+				'src' => $local_image_file_full_url
+			)
+		), $helper->img($local_image_file));
+	}
+	
+	public function test_img__forces_ssl_url_if_local_file() {
+		$settings = $this->getMock('\kin\app\settings');
+		$settings->force_ssl = true;
+		$settings->images_path = 'assets/images/';
+		$settings->url = 'http://leftnode.com/';
+		$settings->secure_url = 'https://leftnode.com/';
+		
+		$helper = new helper;
+		$helper->attach_settings($settings);
+		
+		$local_image_file = 'logo.png';
+		$local_image_file_full_url = $settings->secure_url.$settings->images_path.$local_image_file;
+		
+		$this->assertTag(array(
+			'tag' => 'img',
+			'attributes' => array(
+				'src' => $local_image_file_full_url
+			)
+		), $helper->img($local_image_file));
+	}
+	
+	public function test_img__adds_alt_text() {
+		$settings = $this->getMock('\kin\app\settings');
+		$settings->force_ssl = false;
+		$settings->images_path = 'assets/images/';
+		$settings->url = 'http://leftnode.com/';
+		
+		$helper = new helper;
+		$helper->attach_settings($settings);
+		
+		$local_image_file = 'logo.png';
+		$alt_text = "Welcome to Kin";
+		
+		$this->assertTag(array(
+			'tag' => 'img',
+			'attributes' => array(
+				'alt' => $alt_text,
+				'title' => $alt_text
+			)
+		), $helper->img($local_image_file, $alt_text));
+	}
+	
+	public function test_img__adds_additional_attributes() {
+		$settings = $this->getMock('\kin\app\settings');
+		$settings->force_ssl = false;
+		$settings->images_path = 'assets/images/';
+		$settings->url = 'http://leftnode.com/';
+		
+		$helper = new helper;
+		$helper->attach_settings($settings);
+		
+		$local_image_file = 'logo.png';
+		$styles = 'border: 1px solid black;';
+		$style_attribute = 'style="'.$styles.'"';
+		
+		$this->assertTag(array(
+			'tag' => 'img',
+			'attributes' => array(
+				'style' => $styles
+			)
+		), $helper->img($local_image_file, '', $style_attribute));
+	}
+	
+
+
+	public function test_url__creates_url_from_parameters() {
+		$settings = $this->getMock('\kin\app\settings');
+		$settings->force_ssl = false;
+		$settings->url = 'http://leftnode.com/';
+		$settings->rewrite = false;
+		
+		$helper = new helper;
+		$helper->attach_settings($settings);
+		
+		$url = $settings->url.'index.php/path/to/some/resource';
+		
+		$this->assertEquals($url, $helper->url('path', 'to', 'some', 'resource'));
+	}
+	
+	public function test_url__appends_http_parameters() {
+		$settings = $this->getMock('\kin\app\settings');
+		$settings->force_ssl = false;
+		$settings->url = 'http://leftnode.com/';
+		$settings->rewrite = false;
+		
+		$helper = new helper;
+		$helper->attach_settings($settings);
+		
+		$url = $settings->url.'index.php/path/to/some/resource?id=10&fname=Vic&lname=Cherubini';
+		
+		$this->assertEquals($url, $helper->url('path', 'to', 'some', 'resource', array(
+			'id' => 10,
+			'fname' => 'Vic',
+			'lname' => 'Cherubini'
+		)));
+	}
+	
+	public function test_url__force_ssl_makes_url_secure() {
+		$settings = $this->getMock('\kin\app\settings');
+		$settings->force_ssl = true;
+		$settings->url = 'http://leftnode.com/';
+		$settings->secure_url = 'https://leftnode.com/';
+		$settings->rewrite = false;
+		
+		$helper = new helper;
+		$helper->attach_settings($settings);
+		
+		$url = $settings->secure_url.'index.php/path/to/some/resource';
+		
+		$this->assertEquals($url, $helper->url('path', 'to', 'some', 'resource'));
+	}
+	
+	public function test_url__allows_rewrites() {
+		$settings = $this->getMock('\kin\app\settings');
+		$settings->force_ssl = false;
+		$settings->url = 'http://leftnode.com/';
+		$settings->rewrite = true;
+		
+		$helper = new helper;
+		$helper->attach_settings($settings);
+		
+		$url = $settings->url.'path/to/some/resource';
+		
+		$this->assertEquals($url, $helper->url('path', 'to', 'some', 'resource'));
 	}
 	
 }
