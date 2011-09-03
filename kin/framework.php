0818a2275f6aa5b72142841db2a3e9002d0bb423
Vic Cherubini <vmc@leftnode.com>
2011-09-03 06:25:27 -0500

    Adding the session to the framework include.

--- kin/framework.php
+++ kin/framework.php
@@ -2,6 +2,7 @@
 declare(encoding='UTF-8');
 
 require_once(__DIR__.'/lib/app.php');
+require_once(__DIR__.'/lib/session.php');
 require_once(__DIR__.'/lib/app/controller.php');
 require_once(__DIR__.'/lib/db/model.php');
 require_once(__DIR__.'/lib/db/pdo.php');
