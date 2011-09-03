fe01c3ed95c72ec07fe1e8855a0aa325fe92b11f
Vic Cherubini <vmc@leftnode.com>
2011-09-03 06:26:15 -0500

    On second though, Kin is really built for stateless API's and would not use a session often. I'll keep it out of here and just include it on the front end projects I need it on.

--- kin/framework.php
+++ kin/framework.php
@@ -2,7 +2,6 @@
 declare(encoding='UTF-8');
 
 require_once(__DIR__.'/lib/app.php');
-require_once(__DIR__.'/lib/session.php');
 require_once(__DIR__.'/lib/app/controller.php');
 require_once(__DIR__.'/lib/db/model.php');
 require_once(__DIR__.'/lib/db/pdo.php');
