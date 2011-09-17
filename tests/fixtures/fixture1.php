f4ccee46815fcdbae6e326699ebb190c0de84a15
Vic Cherubini <vmc@leftnode.com>
2011-09-17 05:03:13 -0500

    Added the members of the class here.

--- tests/fixtures/fixture1.php
+++ tests/fixtures/fixture1.php
@@ -1,6 +1,12 @@
 <?php namespace kinfixture;
-declare(encoding='utf-8');
 
 class fixture1 extends \kin\db\model {
-
+	
+	public $id = 0;
+	public $created = '';
+	public $updated = '';
+	public $name = '';
+	public $identifier = '';
+	public $status = 0;
+	
 }
