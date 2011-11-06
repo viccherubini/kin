401dc05e59eeaa23f8523f001c53641959baf878
Vic Cherubini <vmc@leftnode.com>
2011-11-06 14:17:22 -0600

    Removed the bad files from the framework.

--- kin/framework.php
+++ kin/framework.php
@@ -1,6 +1,3 @@
 <?php namespace kin;
-require_once(__DIR__.'/lib/app.php');
-require_once(__DIR__.'/lib/app/controller.php');
-require_once(__DIR__.'/lib/db/model.php');
-require_once(__DIR__.'/lib/db/pdo.php');
-require_once(__DIR__.'/lib/simple_object.php');
+require_once(__DIR__."/lib/app.php");
+require_once(__DIR__."/lib/app/controller.php");
